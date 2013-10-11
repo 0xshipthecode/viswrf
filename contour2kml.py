36141878bbf56e1edbf8665b95e47bb7d9155a65
Martin Vejmelka <vejmelkam@gmail.com>
2013-10-10 22:05:48 -0400

    added contouer2kml.py to render contour 0 as polygons

---
+++ contour2kml.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import netCDF4 as nc4
+import sys
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import simplekml as kml
+
+
+if __name__ == '__main__':
+  
+  if len(sys.argv) != 5:
+    print('Usage: %s <wrfout-file> <varname> <esmf-time> <target-file>' % sys.argv[0])
+    sys.exit(1)
+
+  d = nc4.Dataset(sys.argv[1])
+  varname = sys.argv[2]
+  tstr = sys.argv[3]
+  outf = sys.argv[4]
+
+  # extract ESMF string times
+  times = [''.join(x) for x in d.variables['Times'][:]]
+  if tstr not in times:
+    print('Error: invalid timestamp %s' % tstr)
+    sys.exit(2)
+
+  tndx = times.index(tstr)
+  fa = d.variables[varname][tndx,:,:]
+  lon = d.variables['FXLONG'][0,:,:]
+  lat = d.variables['FXLAT'][0,:,:]
+
+  c = plt.contour(lon, lat, fa, [0]).collections[0]
+  paths = c.get_paths()
+
+  doc = kml.Kml(name = tstr)
+  style = kml.Style()
+  style.linestyle.color = kml.Color.red
+  style.linestyle.width = 3
+  style.polystyle.outline = 1
+  style.polystyle.fill = 0
+  polys = []
+  for path in paths:
+    if len(path) > 5:
+      poly = doc.newpolygon()
+      poly.outerboundaryis = [(x,y) for x,y in path.to_polygons()[0]]
+      poly.style = style
+      polys.append(poly)
+
+  if outf.endswith('kmz'):
+    doc.savekmz(outf)
+  else:
+    doc.save(outf)
+
